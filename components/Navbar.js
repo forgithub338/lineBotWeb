9ee9b611362c6046f2776e458dd65e8f8776ee2d
Alex <forgithub338@gmail.com>
2025-03-03 09:51:00 +0800

    new

---
+++ components/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <nav>
+            這是導覽列
+        </nav>
+    )
+}
---
+++ components/WebFooter.js
@@ -0,0 +1,7 @@
+export default function WebFooter() {
+    return (
+        <footer>
+            這是頁尾
+        </footer>
+    )
+}
